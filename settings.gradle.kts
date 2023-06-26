b039972b93676ab895f7ce9b4d2faa740f0e4736
Pedro Saraiva <pedro.saraiva@sky.uk>
2023-06-26 14:18:55 +0100

    multi module converter

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "test"
+include(":app")
+include(":app:lib")
+include(":app:lib2")
